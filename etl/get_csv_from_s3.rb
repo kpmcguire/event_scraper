ab412bf2e258f6688b1ed17c4d089d565e5175bd
Kevin McGuire <kevin@givegab.com>
2019-05-02 23:43:37 -0400

    read env files

--- etl/get_csv_from_s3.rb
+++ etl/get_csv_from_s3.rb
@@ -1,20 +1,22 @@
 require_relative 'shared'
 
-connection = Fog::Storage.new({
-  provider: 'AWS',
-  aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
-  aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
-  region: ENV['AWS_REGION']
-})
+puts ENV['S3_BUCKET']
 
-directory = connection.directories.get("ithacaeventscalendar")
+# connection = Fog::Storage.new({
+#   provider: 'AWS',
+#   aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+#   aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+#   region: ENV['AWS_REGION']
+# })
 
-directory.files.each do |s3_file|
-  file = File.extname(s3_file.key)
-  if file == ".csv"  
-    File.open("#{Rails.root}/etl/#{s3_file.key}", 'wb+') do |local_file|
-      local_file.write(s3_file.body)
-    end
-  end
-end
+# directory = connection.directories.get("ithacaeventscalendar")
+
+# directory.files.each do |s3_file|
+#   file = File.extname(s3_file.key)
+#   if file == ".csv"  
+#     File.open("#{Rails.root}/etl/#{s3_file.key}", 'wb+') do |local_file|
+#       local_file.write(s3_file.body)
+#     end
+#   end
+# end
 
