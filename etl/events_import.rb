3be088a3307cea68e1b2d8721dc1328156675701
Kevin McGuire <kevin@givegab.com>
2019-05-03 10:37:31 -0400

    read csvs from s3 directly

--- etl/events_import.rb
+++ etl/events_import.rb
@@ -1,8 +1,30 @@
 require_relative 'shared'
 
-Dir.glob(["events_localist_csv/*.csv", "events_ticketfly_csv/*.csv"]).each do |file|
-  CSV.foreach(file, headers: true) do |row|
+connection = Fog::Storage.new({
+  provider: 'AWS',
+  aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+  aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+  region: ENV['AWS_REGION']
+})
+
+def do_it(s3_file)
+  CSV.parse(s3_file.body, headers: true) do |row|
     events_hash = row.to_hash 
     Event.where(name: events_hash['name'], start_time: events_hash['start_time']).first_or_create(events_hash)
   end 
 end
+
+ticketfly_directory = connection.directories.get(ENV['S3_BUCKET'], prefix: 'events_ticketfly_csv/')
+
+localist_directory = connection.directories.get(ENV['S3_BUCKET'], prefix: 'events_localist_csv/')
+
+ticketfly_directory.files.each do |s3_file|
+  do_it(s3_file)
+end
+
+localist_directory.files.each do |s3_file|
+  do_it(s3_file)
+end
+
+
+
--- etl/venues_import.rb
+++ etl/venues_import.rb
@@ -1,10 +1,21 @@
-require_relative '../config/environment'
+require_relative 'shared'
 
-require 'csv'
+connection = Fog::Storage.new({
+  provider: 'AWS',
+  aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+  aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+  region: ENV['AWS_REGION']
+})
 
-Dir.glob("venues_csv/*.csv").each do |file|
-  CSV.foreach(file, headers: true) do |row|
+def do_it(s3_file)
+  CSV.parse(s3_file.body, headers: true) do |row|
     venues_hash = row.to_hash 
     Venue.where(remote_id: venues_hash['remote_id']).first_or_create(venues_hash)
   end 
+end
+
+localist_venues_directory = connection.directories.get(ENV['S3_BUCKET'], prefix: 'venues_csv/')
+
+localist_venues_directory.files.each do |s3_file|
+  do_it(s3_file)
 end
